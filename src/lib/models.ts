bade64f56365dddcdb9d7286546c2dcde16ed275
chivicoko <victor.c.okoye@gmail.com>
2024-07-26 09:33:40 +0100

    update links page, ...

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -1,42 +1,61 @@
-import mongoose from "mongoose";
+import mongoose, { Document, Model } from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        min: 3,
-        max: 20,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        max: 50,
-    },
-    password: {
-        type: String,
-        required: true,
-        min: 8,
-    },
-    img: {
-        type: String,
-    },
-}, {timestamps: true});
-
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    min: 3,
+    max: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+  img: {
+    type: String,
+  },
+}, { timestamps: true });
 
 const userLinkSchema = new mongoose.Schema({
-    platform: {
-      type: String,
-      required: true,
-      enum: ['GitHub', 'YouTube', 'Facebook', 'Twitter', 'LinkedIn', 'Instagram', 'Other'],
-    },
-    userId: {
-      type: String,
-      required: true,
-    },
-  }, { timestamps: true });
-  
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  platform: {
+    type: String,
+    required: true,
+    enum: ['GitHub', 'YouTube', 'Facebook', 'Twitter', 'LinkedIn', 'Instagram', 'Other'],
+  },
+  linkItem: {
+    type: String,
+    required: true,
+  },
+}, { timestamps: true });
 
 export const User = mongoose.models.User || mongoose.model('User', userSchema);
 export const UserLink = mongoose.models.UserLink || mongoose.model('UserLink', userLinkSchema);
+
+interface LinkDocument extends Document {
+  userId: string;
+  platform: string;
+  linkItem: string;
+}
+
+export const getUserLinks = async (): Promise<LinkDocument[]> => {
+  try {
+    const links = await UserLink.find().exec();
+    return links;
+  } catch (error) {
+    console.error("Error fetching user links:", error);
+    throw new Error("Error fetching user links");
+  }
+};
